00069cbfe286a4cb447659fe5f9e902fb5384f24
Egy Maulana <egymaulana777@gmail.com>
2020-05-17 23:39:28 +0700

    added logout function

--- main.py
+++ main.py
@@ -107,14 +107,17 @@ class MainWindow():
                 messagebox.showerror(title = "Error", message = "Username atau Password yang anda masukan salah")
                 mainlogin.destroy()
     def dashboard(self):
+        global dashboard
+        cal = MainWindow()
         dashboard = Toplevel(main)
         dashboard.title("Dashboard")
         dashboard.attributes("-fullscreen", True)
         dashboard.configure(bg = "light cyan")
         Label(dashboard, text = "Dashboard", bg = "spring green", width = "300", height = "2", font = ("Calibri", 20, "bold")).pack()
         Label(dashboard, text = "", bg = "light cyan").pack() #Space
-        Button(dashboard, text = "Log out", height = "2", width = "30", command = dashboard.destroy).pack()
-        
+        Button(dashboard, text = "Log out", height = "2", width = "30", command = cal.Logout).pack()
+    def Logout(self):
+        dashboard.destroy()
 if __name__ == "__main__":
     cal = MainWindow()
     cal.Main()
