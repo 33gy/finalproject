7ad6636c072b36776f4a21f939ffccc19479673f
Egy Maulana <egymaulana777@gmail.com>
2020-05-20 19:25:38 +0700

    added logoutfuction

--- admindashboard.py
+++ admindashboard.py
@@ -1,12 +1,19 @@
 from tkinter import *
-
+import main
 
 class AdminDashboard():
     def AdminDashboard(self):
+        global admindashboard
+        cal = AdminDashboard()
         admindashboard = Tk()
         admindashboard.title("Admin Dashboard")
         admindashboard.attributes("-fullscreen", True)
         admindashboard.configure(bg = "light cyan")
         Label(admindashboard, text = "Admin Dashboard", bg = "spring green", width = "300", height = "2", font = ("Calibri", 20, "bold")).pack()
         Label(admindashboard, text = "", bg = "light cyan").pack() #Space
-        Button(admindashboard, text = "Log out", height = "2", width = "30", command = admindashboard.destroy).pack()
+        Button(admindashboard, text = "Log out", height = "2", width = "30", command = cal.Logout).pack()
+    def Logout(self):
+        admindashboard.destroy()
+        get = main.MainWindow()
+        get.Main()
+        
--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,18 @@
 from tkinter import *
-
+import main
 
 class Dashboard():
     def Dashboard(self):
+        global dashboard
+        cal = Dashboard()
         dashboard = Tk()
         dashboard.title("Dashboard")
         dashboard.attributes("-fullscreen", True)
         dashboard.configure(bg = "light cyan")
         Label(dashboard, text = "Dashboard", bg = "spring green", width = "300", height = "2", font = ("Calibri", 20, "bold")).pack()
         Label(dashboard, text = "", bg = "light cyan").pack() #Space
-        Button(dashboard, text = "Log out", height = "2", width = "30", command = dashboard.destroy).pack()
+        Button(dashboard, text = "Log out", height = "2", width = "30", command = cal.Logout).pack()
+    def Logout(self):
+        dashboard.destroy()
+        get = main.MainWindow()
+        get.Main()
