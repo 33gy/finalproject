56e01382d4738799012be9a62e5541e6052dfe36
Egy Maulana <egymaulana777@gmail.com>
2020-05-21 08:52:02 +0700

    added test button

--- dashboard.py
+++ dashboard.py
@@ -11,7 +11,11 @@ class Dashboard():
         dashboard.configure(bg = "light cyan")
         Label(dashboard, text = "Dashboard", bg = "spring green", width = "300", height = "2", font = ("Calibri", 20, "bold")).pack()
         Label(dashboard, text = "", bg = "light cyan").pack() #Space
+        test = Button(dashboard, text = "test", height = "7", width = "20")
+        test.place(x = 20, y = 100)
+        Label(dashboard, text = "", bg = "light cyan").pack() #Space
         Button(dashboard, text = "Log out", height = "2", width = "30", command = cal.Logout).pack()
+        dashboard.mainloop()
     def Logout(self):
         dashboard.destroy()
         get = main.MainWindow()
