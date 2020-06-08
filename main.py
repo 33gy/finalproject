a4a3e61221183ef2bab509f65714f514441ba51b
Egy Maulana <egymaulana777@gmail.com>
2020-06-08 21:08:56 +0700

    Update main.py
    Change button to themed button

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.ttk as ttk
 import tkinter.messagebox
 import sys
 from login import loginsystem, registsystem
@@ -12,13 +13,20 @@ class MainWindow():
         main = Tk()
         main.title("LibrarySystem")
         main.attributes("-fullscreen", True)
-        Label(text = "LibrarySystem", bg = "spring green", width = "300", height = "2", font = ("Calibri", 20, "bold")).pack()
-        Label(text = "", bg = "light cyan").pack() #Space
-        Button(text = "Login", height = "2", width = "30", command = cal.Login).pack()
-        Label(text = "", bg = "light cyan").pack() #Space
-        Button(text = "Register",height = "2", width = "30", command = cal.Regist).pack()
-        Label(text = "", bg = "light cyan").pack() #Space
-        Button(text = "Exit",height = "2", width = "30", command = cal.Quit).pack()
+        screen_width = main.winfo_screenwidth() # get screen width 
+        screen_height = main.winfo_screenheight() # get screen height
+        top = Label(text = "LibrarySystem", bg = "spring green", font = ("Calibri", 30, "bold")) # header
+        top.place(x = 0, y = 0, width = screen_width, height = screen_height/12) # header placement
+        #text_header = Label(text = "Silahkan pilih menu di bawah", bg = "light cyan", font = ("Calibri", 17)) 
+        #text_header.place(x = screen_width/2.33, y = screen_height/11)
+        login_btn = ttk.Button(text = "Login", width = "30", command = cal.Login) # login button
+        login_btn.place(x = screen_width/2.27, y = screen_height/8, width=230,height=50)
+        #Label(text = "", bg = "light cyan").pack() #Space
+        regist_btn = ttk.Button(text = "Register", width = "30", command = cal.Regist) # register button
+        regist_btn.place(x = screen_width/2.27, y = screen_height/5.5, width=230,height=50)
+        #Label(text = "", bg = "light cyan").pack() #Space
+        exit_btn = ttk.Button(text = "Exit", width = "30", command = cal.Quit) # Exit button
+        exit_btn.place(x = screen_width/2.27, y = screen_height/4.2, width=230,height=50)
         main.configure(bg = "light cyan")
     def Quit(self):
         back = Tk()
