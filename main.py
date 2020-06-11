815cd7b962a097dceeed6804fdbbf0c4b58b7b19
Egy Maulana <egymaulana777@gmail.com>
2020-06-11 20:41:30 +0700

    Update main.py
    delete width

--- main.py
+++ main.py
@@ -19,11 +19,11 @@ class MainWindow():
         top.place(x = 0, y = 0, width = screen_width, height = screen_height/12) # header placement
         #text_header = Label(text = "Silahkan pilih menu di bawah", bg = "light cyan", font = ("Calibri", 17)) 
         #text_header.place(x = screen_width/2.33, y = screen_height/11)
-        login_btn = ttk.Button(text = "Login", width = "30", command = cal.Login) # login button
+        login_btn = ttk.Button(text = "Login", command = cal.Login) # login button
         login_btn.place(x = screen_width/2.27, y = screen_height/8, width=230,height=50)# login_btn placement
-        regist_btn = ttk.Button(text = "Register", width = "30", command = cal.Regist) # register button
+        regist_btn = ttk.Button(text = "Register", command = cal.Regist) # register button
         regist_btn.place(x = screen_width/2.27, y = screen_height/5.5, width=230,height=50) # regist_btn placement
-        exit_btn = ttk.Button(text = "Exit", width = "30", command = cal.Quit) # Exit button
+        exit_btn = ttk.Button(text = "Exit", command = cal.Quit) # Exit button
         exit_btn.place(x = screen_width/2.27, y = screen_height/4.2, width=230,height=50) # exit_btn placement
         main.configure(bg = "light cyan")
     def Quit(self):
