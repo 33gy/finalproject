ff47c97613cf94428dc5ac5f78a5c15ba9a8d79a
Egy Maulana <egymaulana777@gmail.com>
2020-06-24 21:07:44 +0700

    Update caribuku.py
    added 4th tabel coloum for showing lokasi from database

--- dashboard/modul/caribuku.py
+++ dashboard/modul/caribuku.py
@@ -31,13 +31,14 @@ class CariBuku():
         top = Label(text = "Dashboard", bg = "spring green", font = ("Calibri", 30, "bold")) # header
         top.place(x = 0, y = 0, width = screen_width, height = screen_height/12) # header placement
         tabel1 = ttk.Treeview(caribuku, show = "headings")
-        tabel1["column"]=("id", "nama buku","penulis buku", "penerbit buku")
+        tabel1["column"]=("id", "nama buku","penulis buku", "penerbit buku", "lokasi buku")
         tabel1.heading("id", text = "ID")
         tabel1.heading("nama buku", text = "Nama Buku")
         tabel1.heading("penulis buku", text = "Penulis Buku")
         tabel1.heading("penerbit buku", text = "Penerbit Buku")
+        tabel1.heading("lokasi buku", text = "Lokasi Buku")
         tabel1.column("id", width = 20, stretch = NO)
-        tabel1.place(x = screen_width/2.95, y = screen_height/8)
+        tabel1.place(x = screen_width/3.4, y = screen_height/8)
         search_label = Label(caribuku, text = "Search",  bg = "light cyan", font = ("Calibri", 20))
         search_label.place(x = screen_width/2.10, y = screen_height/2.97)
         search_entry = ttk.Entry(caribuku)
@@ -61,7 +62,7 @@ class CariBuku():
         a = curr.fetchall()
         i = 0
         for row in a:
-            tabel1.insert("","end", text=str(i), values=(row[0], row[1], row[2], row[3]))
+            tabel1.insert("","end", text=str(i), values=(row[0], row[1], row[2], row[3], row[4]))
             i +=1
         mydb.commit()
     def clear(self):
@@ -71,10 +72,10 @@ class CariBuku():
         get = search_entry.get()
         for i in tabel1.get_children():
            tabel1.delete(i)
-        curr.execute("SELECT * FROM daftar_buku WHERE namabuku = %s or penulisbuku = %s or penerbitbuku = %s",(get,get,get))
+        curr.execute("SELECT * FROM daftar_buku WHERE namabuku = %s or penulisbuku = %s or penerbitbuku = %s or lokasi = %s",(get, get, get, get))
         a = curr.fetchall()
         i = 0
         for row in a:
-            tabel1.insert("","end", text=str(i), values=(row[0], row[1], row[2], row[3]))
+            tabel1.insert("","end", text=str(i), values=(row[0], row[1], row[2], row[3], row[4]))
             i += 1
         mydb.commit()
